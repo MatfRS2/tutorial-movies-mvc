b4cc9c66283895b8c793912f8b218003f7c4708b
Vladimir Filipovic <vladofilipovic@hotmail.com>
2022-10-01 18:12:44 +0200

    dodao ZdravoSvete kontroler

---
+++ MvcMovies/Controllers/ZdravoSveteController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class ZdravoSveteController : Controller
+    {
+        // 
+        // GET: /ZdravoSvete/
+
+        public string Index()
+        {
+            return "Ovo je metod za podrazumevanu akciju...";
+        }
+
+        // 
+        // GET: /ZdravoSvete/DobroDosli/ 
+
+        public string Dobrodosli()
+        {
+            return "Ovo je metod za akciju dobrodošlice (DobroDosli)...";
+        }
+    }
+}
