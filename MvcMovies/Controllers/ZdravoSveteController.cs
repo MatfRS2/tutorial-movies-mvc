1df6185803d8fdfea4187ebd8b3c7f22090ed31b
Vladimir Filipovic <vladofilipovic@hotmail.com>
2022-10-01 18:22:20 +0200

    kontroler, metod, parametri

--- MvcMovies/Controllers/ZdravoSveteController.cs
+++ MvcMovies/Controllers/ZdravoSveteController.cs
@@ -7,7 +7,6 @@ namespace MvcMovie.Controllers
     {
         // 
         // GET: /ZdravoSvete/
-
         public string Index()
         {
             return "Ovo je metod za podrazumevanu akciju...";
@@ -15,10 +14,18 @@ namespace MvcMovie.Controllers
 
         // 
         // GET: /ZdravoSvete/DobroDosli/ 
-
         public string Dobrodosli()
         {
             return "Ovo je metod za akciju dobrodošlice (DobroDosli)...";
         }
+
+        // 
+        // GET: /ZdravoSvete/DobroDosli2/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Dobrodosli2(string ime, int brojPokusaja = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {ime}, broj pokusaja je: {brojPokusaja}");
+        }
+
     }
 }
