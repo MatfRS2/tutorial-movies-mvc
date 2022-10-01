56c6150cd66a657e4ba54a006f7b855cd4ce6905
Vladimir Filipovic <vladofilipovic@hotmail.com>
2022-10-01 22:05:56 +0200

    podesio poglede i zajednicki izgled

---
+++ MvcMovies/Controllers/FilmoviController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class FilmoviController : Controller
+    {
+        // 
+        // GET: /ZdravoSvete/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /ZdravoSvete/DobroDosli/ 
+        public string Dobrodosli()
+        {
+            return "Ovo je metod za akciju dobrodošlice (DobroDosli)...";
+        }
+
+        // 
+        // GET: /ZdravoSvete/DobroDosli2/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Dobrodosli2(string ime, int brojPokusaja = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Doborodosli, {ime}, broj pokusaja je: {brojPokusaja}");
+        }
+
+        // 
+        // GET: /ZdravoSvete/DobroDosli3/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Dobrodosli3(string ime, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Dobrodosli {ime}, broj pokusaja je: {ID}");
+        }
+    }
+}
--- MvcMovies/Controllers/HomeController.cs
+++ MvcMovies/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace MvcMovies.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Privatnost()
         {
             return View();
         }
