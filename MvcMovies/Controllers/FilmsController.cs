eb64c01c9f5e42a77b1d75b12abd0f9c4b86e110
Vladimir Filipovic <vladofilipovic@hotmail.com>
2022-10-11 12:02:19 +0200

    dodao model i izvrsio scaffold data zataj model

---
+++ MvcMovies/Controllers/FilmsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovies.Data;
+using MvcMovies.Models;
+
+namespace MvcMovies.Controllers
+{
+    public class FilmsController : Controller
+    {
+        private readonly MvcMoviesContext _context;
+
+        public FilmsController(MvcMoviesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Films
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Film.ToListAsync());
+        }
+
+        // GET: Films/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Film == null)
+            {
+                return NotFound();
+            }
+
+            var film = await _context.Film
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return View(film);
+        }
+
+        // GET: Films/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Films/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Naslov,DatumPocetkaPrikazivanja,Zanr,Ulozeno")] Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(film);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(film);
+        }
+
+        // GET: Films/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Film == null)
+            {
+                return NotFound();
+            }
+
+            var film = await _context.Film.FindAsync(id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+            return View(film);
+        }
+
+        // POST: Films/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Naslov,DatumPocetkaPrikazivanja,Zanr,Ulozeno")] Film film)
+        {
+            if (id != film.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(film);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FilmExists(film.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(film);
+        }
+
+        // GET: Films/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Film == null)
+            {
+                return NotFound();
+            }
+
+            var film = await _context.Film
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return View(film);
+        }
+
+        // POST: Films/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Film == null)
+            {
+                return Problem("Entity set 'MvcMoviesContext.Film'  is null.");
+            }
+            var film = await _context.Film.FindAsync(id);
+            if (film != null)
+            {
+                _context.Film.Remove(film);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FilmExists(int id)
+        {
+          return _context.Film.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MvcMovies/Migrations/20221011094313_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovies.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Film",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naslov = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DatumPocetkaPrikazivanja = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Zanr = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ulozeno = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Film", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Film");
+        }
+    }
+}
